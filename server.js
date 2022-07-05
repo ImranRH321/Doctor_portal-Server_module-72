bad24d2658da349b2ef39c7ed271f2e147ace467
ImranRH321 <imranrh247@gmail.com>
2022-07-05 16:48:20 +0600

    Send JWT token to back end for verification

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ConnectionCheckOutStartedEvent } = require("mongodb");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -50,7 +51,8 @@ async function run() {
         $set: user
       }
       const result = await userCollection.updateOne(filter, updateDoc, option)
-     res.send(result)
+      const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN,  { expiresIn: '6h' })
+     res.send({result, token})
     })
     
     /* ..... */
